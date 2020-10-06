dd7e1d49c92b385f283548b04ba28d57dde050fe
gato <alex.n.parshikov@gmail.com>
2020-10-06 20:44:36 +0300

    added comparison of YAML files

--- generate_diff/generate_diff.py
+++ generate_diff/generate_diff.py
@@ -1,12 +1,9 @@
 import json
-from os.path import abspath
+import yaml
+from os.path import abspath, splitext
 
 
-def gendiff(first_file, second_file):
-    with open(first_file, 'r') as ff:
-        first_data = json.load(ff)
-    with open(second_file, 'r') as sf:
-        second_data = json.load(sf)
+def gendiff(first_data, second_data):
     result_diff = {}
     for key, value in second_data.items():
         if key in first_data:
@@ -23,10 +20,33 @@ def gendiff(first_file, second_file):
     return result_diff
 
 
-def get_diff(path_1, path_2):
+def load_from_file(user_file, file_format):
+    with open(user_file, 'r') as file_load:
+        if file_format == '.json':
+            return json.load(file_load)
+        elif file_format == '.yaml' or file_format == '.yml':
+            return yaml.load(file_load, yaml.SafeLoader)
+
+
+def get_diff(path_to_file1, path_to_file2):
+    full_path1 = abspath(path_to_file1)
+    full_path2 = abspath(path_to_file2)
+    first_data = load_from_file(
+        full_path1,
+        splitext(full_path1)[1],
+    )
+    second_data = load_from_file(
+        full_path2,
+        splitext(full_path2)[1],
+    )
+    print('=======================================')
+    print(first_data)
+    print('=======================================')
+    print(second_data)
+    print('=======================================')
     result = gendiff(
-        abspath(path_1),
-        abspath(path_2),
+        first_data,
+        second_data,
         )
     print('{')
     for key, value in result.items():
--- tests/test_diff_files.py
+++ tests/test_diff_files.py
@@ -1,4 +1,4 @@
-from generate_diff.generate_diff import gendiff
+from generate_diff.generate_diff import gendiff, load_from_file
 
 
 def test_diff():
@@ -10,6 +10,36 @@ def test_diff():
         'proxy': '123.234.53.22',
         'follow': False,
         }
-    first_file = 'tests/fixtures/file1.json'
-    second_file = 'tests/fixtures/file2.json'
-    assert gendiff(first_file, second_file) == diff_data
+    first_data = {
+        "host": "hexlet.io",
+        "timeout": 50,
+        "proxy": "123.234.53.22",
+        "follow": False,
+    }
+    second_data = {
+        "timeout": 20,
+        "verbose": True,
+        "host": "hexlet.io",
+    }
+    assert gendiff(first_data, second_data) == diff_data
+
+
+def test_load_json_file():
+    test_dict1 = {
+        'host': 'hexlet.io',
+        'timeout': 50,
+        'proxy': '123.234.53.22',
+        'follow': False,
+        }
+    file_for_test = 'tests/fixtures/file1.json'
+    assert load_from_file(file_for_test, '.json') == test_dict1
+
+
+def test_load_yaml_file():
+    test_dict2 = {
+        'timeout': 20,
+        'verbose': True,
+        'host': 'hexlet.io',
+    }
+    file_for_test = 'tests/fixtures/other_file2.yaml'
+    assert load_from_file(file_for_test, '.yaml') == test_dict2
